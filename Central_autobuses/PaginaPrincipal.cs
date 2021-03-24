d3d87032addd2bcab4060769057759e50ac6dd84
Alejandro <alejandrovalenzuela051@gmail.com>
2021-03-24 17:10:20 -0600

    Cambio de texto de la barra automatizado

--- Central_autobuses/PaginaPrincipal.cs
+++ Central_autobuses/PaginaPrincipal.cs
@@ -24,6 +24,7 @@ namespace Central_autobuses
                     AutoScroll = false
                 };
 
+                Text = "Central | Comprar boleto";
                 PanelMenus.Controls.Add(PantallaBoleto);
                 PantallaBoleto.Show();
             }
