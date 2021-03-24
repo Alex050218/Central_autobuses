a11b7fded5bb74c88bf373857a8e0c66e4d9f910
Alejandro <alejandrovalenzuela051@gmail.com>
2021-03-24 16:29:44 -0600

    Creada pantalla de bienvenida

--- Central_autobuses/PaginaPrincipal.Designer.cs
+++ Central_autobuses/PaginaPrincipal.Designer.cs
@@ -33,7 +33,13 @@ namespace Central_autobuses
             this.panel1 = new System.Windows.Forms.Panel();
             this.BtnPasajeros = new System.Windows.Forms.Button();
             this.PanelMenus = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
+            this.PanelMenus.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // BtnComprarBoleto
@@ -67,11 +73,54 @@ namespace Central_autobuses
             // 
             // PanelMenus
             // 
+            this.PanelMenus.Controls.Add(this.label3);
+            this.PanelMenus.Controls.Add(this.pictureBox1);
+            this.PanelMenus.Controls.Add(this.label2);
+            this.PanelMenus.Controls.Add(this.label1);
             this.PanelMenus.Dock = System.Windows.Forms.DockStyle.Fill;
             this.PanelMenus.Location = new System.Drawing.Point(0, 0);
             this.PanelMenus.Name = "PanelMenus";
             this.PanelMenus.Size = new System.Drawing.Size(800, 359);
             this.PanelMenus.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(194, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(438, 73);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "BIENVENIDO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(414, 170);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(206, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "¿Que haremos hoy?";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Central_autobuses.Properties.Resources.Logo;
+            this.pictureBox1.Location = new System.Drawing.Point(207, 142);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(192, 129);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(416, 195);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(157, 39);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Seleccione una de las opciones de abajo";
             // 
             // PaginaPrincipal
             // 
@@ -86,6 +135,9 @@ namespace Central_autobuses
             this.Name = "PaginaPrincipal";
             this.Text = "Central | Pagina Principal";
             this.panel1.ResumeLayout(false);
+            this.PanelMenus.ResumeLayout(false);
+            this.PanelMenus.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -96,6 +148,10 @@ namespace Central_autobuses
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button BtnPasajeros;
         private System.Windows.Forms.Panel PanelMenus;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
