a5f1f60afaf27644bc8bfebefc2ff2a705b093fd
Alejandro <alejandrovalenzuela051@gmail.com>
2021-03-28 09:41:20 -0600

    Titulo agregado

--- Central_autobuses/Pasajeros.Designer.cs
+++ Central_autobuses/Pasajeros.Designer.cs
@@ -35,13 +35,14 @@ namespace Central_autobuses
             this.lblMaza = new System.Windows.Forms.Label();
             this.lblLuis = new System.Windows.Forms.Label();
             this.lblCiudad = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(273, 119);
+            this.label1.Location = new System.Drawing.Point(262, 119);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(100, 25);
             this.label1.TabIndex = 0;
@@ -51,7 +52,7 @@ namespace Central_autobuses
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(211, 170);
+            this.label2.Location = new System.Drawing.Point(200, 170);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(162, 25);
             this.label2.TabIndex = 1;
@@ -61,7 +62,7 @@ namespace Central_autobuses
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(188, 216);
+            this.label3.Location = new System.Drawing.Point(177, 216);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(185, 25);
             this.label3.TabIndex = 2;
@@ -71,7 +72,7 @@ namespace Central_autobuses
             // 
             this.lblMaza.AutoSize = true;
             this.lblMaza.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMaza.Location = new System.Drawing.Point(401, 119);
+            this.lblMaza.Location = new System.Drawing.Point(529, 119);
             this.lblMaza.Name = "lblMaza";
             this.lblMaza.Size = new System.Drawing.Size(36, 25);
             this.lblMaza.TabIndex = 3;
@@ -81,7 +82,7 @@ namespace Central_autobuses
             // 
             this.lblLuis.AutoSize = true;
             this.lblLuis.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblLuis.Location = new System.Drawing.Point(401, 170);
+            this.lblLuis.Location = new System.Drawing.Point(529, 170);
             this.lblLuis.Name = "lblLuis";
             this.lblLuis.Size = new System.Drawing.Size(36, 25);
             this.lblLuis.TabIndex = 4;
@@ -91,17 +92,29 @@ namespace Central_autobuses
             // 
             this.lblCiudad.AutoSize = true;
             this.lblCiudad.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCiudad.Location = new System.Drawing.Point(401, 216);
+            this.lblCiudad.Location = new System.Drawing.Point(529, 216);
             this.lblCiudad.Name = "lblCiudad";
             this.lblCiudad.Size = new System.Drawing.Size(36, 25);
             this.lblCiudad.TabIndex = 5;
             this.lblCiudad.Text = "40";
+            // 
+            // label4
+            // 
+            this.label4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(0, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(800, 75);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Asientos disponibles";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Pasajeros
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 359);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.lblCiudad);
             this.Controls.Add(this.lblLuis);
             this.Controls.Add(this.lblMaza);
@@ -125,5 +138,6 @@ namespace Central_autobuses
         private System.Windows.Forms.Label lblMaza;
         private System.Windows.Forms.Label lblLuis;
         private System.Windows.Forms.Label lblCiudad;
+        private System.Windows.Forms.Label label4;
     }
 }
